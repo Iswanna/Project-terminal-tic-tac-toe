759331ce49488b4185010a23fb6d474c3092e3ba
iswat <ibeetek@gmail.com>
2025-11-18 18:13:10 +0000

    Add failing test case for printBoard function

--- __tests__/board-printer.test.js
+++ __tests__/board-printer.test.js
@@ -1,6 +1,24 @@
-import { checkIfNoMovesLeft } from "../board-printer";
+import { checkIfNoMovesLeft, printBoard } from "../board-printer";
 
 test("checkIfNoMovesLeft should return true if there are no moves left", () => {
   const board = [];
   expect(checkIfNoMovesLeft(board)).toBe(true);
+});
+
+test.only("printBoard should return a formatted board string", () => {
+  const board = [
+    ["X", "_", "_"],
+    ["_", "X", "_"],
+    ["O", "O", "X"],
+  ];
+
+  const expected = `
+  X  |     |     
+=================
+     |  X  |     
+=================
+  O  |  O  |  X  
+=================`;
+
+  expect(printBoard(board)).toEqual(expected);
 });
